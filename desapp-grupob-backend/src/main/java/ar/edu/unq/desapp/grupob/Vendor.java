678de88eabaf65275fa2bbc159e3c01e6d19e776
Marcelo Rubini <rubini.marcelo@gmail.com>
2014-09-08 12:06:17 -0300

    Logica (precaria?) de Carga de Comprobantes. Agregados: Invoice InvoiceType Vendor

---
+++ desapp-grupob-backend/src/main/java/ar/edu/unq/desapp/grupob/Vendor.java
@@ -0,0 +1,31 @@
+package ar.edu.unq.desapp.grupob;
+
+public class Vendor {
+
+    public Vendor() {
+        
+    }
+    
+    /*
+     * COMPORTAMIENTO PARA CALCULAR LOS PORCENTAJES DE IVA
+     * QUE CORRESPONDEN A PARTIR DE LA CONDICION DEL PROVEEDOR
+     * Y LA DEL NEGOCIO DEL USUARIO
+     *  
+     */
+    public int getTaxFromA() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    public int getTaxFromB() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    public int getTaxFromC() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    public int getTaxFromX() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+}
