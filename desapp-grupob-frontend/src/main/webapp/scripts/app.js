35127a3cf0d3d4f84e90fbaf6ff657f06bbfdedc
Marcelo Rubini <rubini.marcelo@gmail.com>
2014-11-04 21:23:08 -0300

    Mappings completos

--- desapp-grupob-frontend/src/main/webapp/scripts/app.js
+++ desapp-grupob-frontend/src/main/webapp/scripts/app.js
@@ -12,49 +12,65 @@ var app = angular.module(
 		[ 'ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize',
 				'ngTouch', 'ngGrid' ]).config(function($routeProvider) {
 	$routeProvider
-
+	
+//	//ACCOUNTS
+//	.when('/accounts', {
+//		templateUrl : 'views/accounts.html',
+//		controller : 'AccountControllerList'
+//	}).when('/newAccount', {
+//		templateUrl : 'views/account.html',
+//		controller : 'AccountControllerNew'
+//	})
+//	EN PRINCIPIO ESTO NO TIENE SENTIDO
+//	.when('/editAccount/:accountId', {			
+//		templateUrl : 'views/account.html',
+//		controller : 'AccountControllerEdit'
+//	}).when('/deleteAccount/:accountId', {
+//		controller : 'AccountControllerDelete'
+//	})	
+	
 	// OPERATIONS
 	.when('/operations', {
-	templateUrl : 'views/operations.html',
-	controller : 'OperationControllerList'
+		templateUrl : 'views/operations.html',
+		controller : 'OperationControllerList'
 	}).when('/newOperation', {
-	templateUrl : 'views/operation.html',
-	controller : 'OperationControllerNew'
+		templateUrl : 'views/operation.html',
+		controller : 'OperationControllerNew'
 	}).when('/editOperation/:operationId', {
-	templateUrl : 'views/operation.html',
-	controller : 'CategoryControllerEdit'
+		templateUrl : 'views/operation.html',
+		controller : 'CategoryControllerEdit'
 	}).when('/deleteOperation/:operationId', {
-	controller : 'CategoryControllerDelete'
+		controller : 'CategoryControllerDelete'
 	})
 	// CATEGORIES
 	.when('/categories', {
-	templateUrl : 'views/categories.html',
-	controller : 'CategoryControllerList'
+		templateUrl : 'views/categories.html',
+		controller : 'CategoryControllerList'
 	}).when('/newCategory', {
-	templateUrl : 'views/category.html',
-	controller : 'CategoryControllerNew'
+		templateUrl : 'views/category.html',
+		controller : 'CategoryControllerNew'
 	}).when('/editCategory/:categoryId', {
-	templateUrl : 'views/category.html',
-	controller : 'CategoryControllerEdit'
+		templateUrl : 'views/category.html',
+		controller : 'CategoryControllerEdit'
 	}).when('/deleteCategory/:categoryId', {
-	controller : 'CategoryControlleDelete'
+		controller : 'CategoryControlleDelete'
 	})
 	// INVOICES
 	.when('/invoices', {
-	templateUrl : 'views/invoices.html',
-	controller : 'InvoiceControllerList'
+		templateUrl : 'views/invoices.html',
+		controller : 'InvoiceControllerList'
 	}).when('/newInvoice', {
-	templateUrl : 'views/invoices.html',
-	controller : 'InvoiceControllerNew'
+		templateUrl : 'views/invoices.html',
+		controller : 'InvoiceControllerNew'
 	}).when('/editInvoice/:invoiceId', {
-	templateUrl : 'views/invoices.html',
-	controller : 'InvoiceControllerEdit'
+		templateUrl : 'views/invoices.html',
+		controller : 'InvoiceControllerEdit'
 	}).when('/deleteInvoice/:invoiceId', {
-	controller : 'InvoiceControllerDelete'
+		controller : 'InvoiceControllerDelete'
 	})
 	// OTHERWISE
 	.otherwise({
-	redirectTo : '/'
+		redirectTo : '/'
 	});
 
 });
