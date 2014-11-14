60a3b8c63e9d94d84ab3435382721b86c8f7cd82
dgonzalia <dgonzalia@gmail.com>
2014-11-14 14:03:37 -0300

    ABM de categorias funcionando con get, put, post y delete

--- desapp-grupob-backend/src/main/java/ar/edu/unq/desapp/grupob/web/rest/CategoryRest.java
+++ desapp-grupob-backend/src/main/java/ar/edu/unq/desapp/grupob/web/rest/CategoryRest.java
@@ -2,11 +2,21 @@ package ar.edu.unq.desapp.grupob.web.rest;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.apache.cxf.feature.Features;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.edu.unq.desapp.grupob.model.Category;
@@ -14,10 +24,19 @@ import ar.edu.unq.desapp.grupob.services.CategoryService;
 
 @Service
 @Path("/categoryService")
+@Features(features = "org.apache.cxf.feature.LoggingFeature") 
 public class CategoryRest {
 	
-    private CategoryService categoryService;
+    private static final int HTTP_OK_CREATED = 201;
 
+    private static final int HTTP_OK = 200;
+
+    private static final int HTTP_DELETE = 204;
+
+    
+    @Autowired
+    private CategoryService categoryService;
+    
     @GET
     @Path("/categories")
     @Produces("application/json")
@@ -29,8 +48,7 @@ public class CategoryRest {
     @Path("/category/{id}")
     @Produces("application/json")
     public Category getCategory(@PathParam("id") final int id) {
-        Category category = getCategoryService().getById(id);
-        return category;
+        return getCategoryService().getById(id) ;
     }
 
     @GET
@@ -40,34 +58,66 @@ public class CategoryRest {
         return getCategoryService().filterByName(name);
     }
 
-    @GET
-    @Path("/save/{name}")
-    @Produces("application/json")
-    public Category saveCategory(@PathParam("name") final String name) {
-        Category category = new Category(name);
-        getCategoryService().save(category);
-        return category;
+    @POST
+    @Path("/save")
+    @Consumes("application/json")
+    public Response saveCategory(@Multipart(value = "category", type = "application/json") final String jsonCategory) {
+        try {
+            getCategoryService().save(parseCategory(jsonCategory));
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+        return Response.status(HTTP_OK_CREATED).build();
     }
     
-    @GET
-    @Path("/update/{id}/{name}")
-    @Produces("application/json")
-    public Category updateCategory(@PathParam("id") final int id,
-            @PathParam("name") final String name) {
-        Category category = getCategoryService().getById(id);
-        getCategoryService().update(category);
-        return category;
+    @PUT
+    @Path("/{categoryId}")
+    @Consumes("application/json")
+    public Response updateBook(@PathParam("categoryId") final String categoryId, final String jsonCategory) {
+        try {
+            Category older = getCategoryService().getById(new Integer (categoryId));
+            System.out.println("Id y nombre de la categoria a updatear 'vieja': "+older.getId()+ " - "+older.getName());
+            Category newer = parseCategory(jsonCategory);
+            System.out.println("Id y nombre de la categoria a updatear 'nueva': "+newer.getId()+ " - "+newer.getName());
+            getCategoryService().update(newer);
+        } catch (Exception e) {
+            return Response.status(Status.METHOD_NOT_ALLOWED).build();
+        }
+        return Response.ok().status(HTTP_OK).build();
+    }
+    
+//    @POST
+//    @Path("/update")
+//    @Consumes("application/json")
+//    public Response updateBook(@Multipart(value = "category", type = "application/json") final String jsonCategory) {
+//        try {
+//            getCategoryService().update(parseCategory(jsonCategory));
+//        } catch (Exception e) {
+//            return Response.status(Status.METHOD_NOT_ALLOWED).build();
+//        }
+//        return Response.ok().status(HTTP_OK).build();
+//    }
+    
+    @DELETE
+    @Path("/{categoryId}")
+    public Response deleteBook(@PathParam("categoryId") final String categoryId) {
+        try {
+            Category categoryToBeDeleted = getCategoryService().getById(new Integer(categoryId));
+            getCategoryService().delete(categoryToBeDeleted);
+        } catch (Exception e) {
+            return Response.status(Status.CONFLICT).build();
+        }
+        return Response.ok().status(HTTP_DELETE).build();
     }
 
-    @GET
-    @Path("/delete/{id}")
-    @Produces("application/json")
-    public Category deleteCategory(@PathParam("id") final int id) {
-        Category category = this.getCategory(id);
-        getCategoryService().delete(category);
-        return category;
-    }
 
+    private Category parseCategory(final String jasonCategory) throws Exception {
+        Category newCategory = new Category();
+        ObjectMapper mapper = new ObjectMapper();
+        newCategory = mapper.readValue(jasonCategory, Category.class);
+        return newCategory;
+    }
+    
     public CategoryService getCategoryService() {
         return categoryService;
     }
--- desapp-grupob-frontend/src/main/webapp/scripts/app.js
+++ desapp-grupob-frontend/src/main/webapp/scripts/app.js
@@ -7,75 +7,90 @@
  * 
  * Main module of the application.
  */
-var app = angular.module(
-		'app',
-		[ 'ngAnimate', 'ngCookies', 'ngResource', 'ngRoute', 'ngSanitize',
-				'ngTouch', 'ngGrid' ]).config(function($routeProvider) {
-	$routeProvider
-	
-//	//ACCOUNTS
-//	.when('/accounts', {
-//		templateUrl : 'views/accounts.html',
-//		controller : 'AccountControllerList'
-//	}).when('/newAccount', {
-//		templateUrl : 'views/account.html',
-//		controller : 'AccountControllerNew'
-//	})
-//	EN PRINCIPIO ESTO NO TIENE SENTIDO
-//	.when('/editAccount/:accountId', {			
-//		templateUrl : 'views/account.html',
-//		controller : 'AccountControllerEdit'
-//	}).when('/deleteAccount/:accountId', {
-//		controller : 'AccountControllerDelete'
-//	})	
-	
-	// OPERATIONS
-	.when('/operations', {
-		templateUrl : 'views/operations.html',
-		controller : 'OperationControllerList'
-	}).when('/newOperation', {
-		templateUrl : 'views/operation.html',
-		controller : 'OperationControllerNew'
-	}).when('/editOperation/:operationId', {
-		templateUrl : 'views/operation.html',
-		controller : 'CategoryControllerEdit'
-	}).when('/deleteOperation/:operationId', {
-		controller : 'CategoryControllerDelete'
-	})
-	// CATEGORIES
-	.when('/categories', {
-		templateUrl : 'views/categories.html',
-		controller : 'CategoryControllerList'
-	}).when('/newCategory', {
-		templateUrl : 'views/category.html',
-		controller : 'CategoryControllerNew'
-	}).when('/editCategory/:categoryId', {
-		templateUrl : 'views/category.html',
-		controller : 'CategoryControllerEdit'
-	}).when('/deleteCategory/:categoryId', {
-		controller : 'CategoryControlleDelete'
-	})
-	// INVOICES
-	.when('/invoices', {
-		templateUrl : 'views/invoices.html',
-		controller : 'InvoiceControllerList'
-	}).when('/newInvoice', {
-		templateUrl : 'views/invoices.html',
-		controller : 'InvoiceControllerNew'
-	}).when('/editInvoice/:invoiceId', {
-		templateUrl : 'views/invoices.html',
-		controller : 'InvoiceControllerEdit'
-	}).when('/deleteInvoice/:invoiceId', {
-		controller : 'InvoiceControllerDelete'
-	})
-	// OTHERWISE
-	.otherwise({
-		redirectTo : '/'
-	});
+var feag = angular.module('feag', [ 'ngAnimate', 'ngCookies', 'ngResource',
+		'ngRoute', 'ngSanitize', 'ngTouch', 'ngGrid', 'xeditable' ]);
 
+feag.config([ '$routeProvider', '$locationProvider',
+		function($routeProvider, $locationProvider) {
+			$routeProvider
+
+			// //ACCOUNTS
+			// .when('/accounts', {
+			// templateUrl : 'views/accounts.html',
+			// controller : 'AccountControllerList'
+			// }).when('/newAccount', {
+			// templateUrl : 'views/account.html',
+			// controller : 'AccountControllerNew'
+			// })
+			// EN PRINCIPIO ESTO NO TIENE SENTIDO
+			// .when('/editAccount/:accountId', {
+			// templateUrl : 'views/account.html',
+			// controller : 'AccountControllerEdit'
+			// }).when('/deleteAccount/:accountId', {
+			// controller : 'AccountControllerDelete'
+			// })
+
+			// OPERATIONS
+			.when('/operations', {
+				templateUrl : 'views/operations.html',
+				controller : 'OperationControllerList'
+			}).when('/newOperation', {
+				templateUrl : 'views/operation.html',
+				controller : 'OperationControllerNew'
+			}).when('/editOperation/:operationId', {
+				templateUrl : 'views/operation.html',
+				controller : 'CategoryControllerEdit'
+			}).when('/deleteOperation/:operationId', {
+				controller : 'CategoryControllerDelete'
+			})
+
+			// Categories ABM
+
+			.when('/categoriesABM', {
+				templateUrl : 'views/categoriesABM.html',
+				controller : 'categoriesCtrl'
+			})
+			// CATEGORIES
+			.when('/categories', {
+				templateUrl : 'views/categories.html',
+				controller : 'categoryControllerList'
+			})
+
+			.when('/newCategory', {
+				templateUrl : 'views/category.html',
+				controller : 'CategoryControllerNew'
+			}).when('/editCategory/:categoryId', {
+				templateUrl : 'views/category.html',
+				controller : 'CategoryControllerEdit'
+			}).when('/deleteCategory/:categoryId', {
+				controller : 'CategoryControlleDelete'
+			})
+			// INVOICES
+			.when('/invoices', {
+				templateUrl : 'views/invoices.html',
+				controller : 'InvoiceControllerList'
+			}).when('/newInvoice', {
+				templateUrl : 'views/invoices.html',
+				controller : 'InvoiceControllerNew'
+			}).when('/editInvoice/:invoiceId', {
+				templateUrl : 'views/invoices.html',
+				controller : 'InvoiceControllerEdit'
+			}).when('/deleteInvoice/:invoiceId', {
+				controller : 'InvoiceControllerDelete'
+			})
+			// OTHERWISE
+			.otherwise({
+				redirectTo : '/'
+			});
+
+		} ]);
+
+feag.run(function(editableOptions) {
+	editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2',
+									// 'default'
 });
 
-app.factory("alert", function($window, $q) {
+feag.factory("alert", function($window, $q) {
 
 	// Define promise-based alert() method.
 	function alert(message) {
